0e2cebcd04ec6661d64e8bcfacbcb5adf2a160e1
zkan <kan@prontomarketing.com>
2017-10-26 11:13:54 +0700

    Remove integrations app

--- django_google_sheets_export/integrations/models.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.db import models
-
-
-class Integration(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL
-    )
-    access_token = models.CharField(max_length=500)
